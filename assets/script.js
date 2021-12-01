3e5407acde7bc6a7c0d00ecc3240709230d7189d
Alex <nypels.alexandre@outlook.com>
2021-12-01 18:10:15 +0000

    adding operators handler function

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// Theme Changer Function
 document.getElementById("theme_changer").addEventListener("change", (e) => {
     const {value} = e.target
     const body = document.getElementById("body")
@@ -15,3 +16,76 @@ document.getElementById("theme_changer").addEventListener("change", (e) => {
             break;
     }
 })
+
+
+const calculator = {
+    firstOperand: null,
+    operator: null,
+    secondOperand: false,
+    valueToDisplay: '0'
+}
+
+let displayValue = () => {
+    const display = document.getElementById("display")
+    display.value = calculator.valueToDisplay
+}
+
+let inputDigit = (value) => {
+    const {valueToDisplay, secondOperand} = calculator
+    if(secondOperand == false) {
+        calculator.valueToDisplay = valueToDisplay === "0" ? value : valueToDisplay + value
+    }else{
+        calculator.secondOperand = false
+        calculator.valueToDisplay = value
+    }
+}
+
+let inputDecimal = (dot) => {
+    const {valueToDisplay} = calculator
+    if(!valueToDisplay.includes(dot)) {
+        calculator.valueToDisplay = valueToDisplay + dot
+    }
+}
+
+let handleOperator = (key) => {
+    const {firstOperand, valueToDisplay} = calculator
+    const inputValue = parseFloat(valueToDisplay)
+    if(firstOperand == null && !isNaN(inputValue)) {
+        calculator.firstOperand = inputValue
+    }
+    calculator.operator = key
+    calculator.secondOperand = true
+}
+
+const keysContainer = document.querySelector(".keys-container")
+keysContainer.addEventListener("click", (e) => {
+    const {target} = e
+    if(!target.matches("button")) {
+        return
+    }
+    if(target.classList.contains("decimal")) {
+        inputDecimal(target.value)
+        displayValue()
+        return
+    }
+    if(target.classList.contains("clear")) {
+        console.log("All clear: " + target.value);
+        return
+    }
+    if(target.classList.contains("delete")) {
+        console.log(("Del: " + target.value));
+        return
+    }
+    if(target.classList.contains("operator")) {
+        handleOperator(target.value)
+        return
+    }
+    if(target.classList.contains("keys")) {
+        inputDigit(target.value)
+        displayValue()
+        return
+    }
+})
+
+
+displayValue()
